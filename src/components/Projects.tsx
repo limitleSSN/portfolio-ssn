cb17cda035f61e9d0e5fab81d8d6e4f223ed8780
limitleSSN <soumyasouravssn@gmail.com>
2025-06-03 21:29:05 +0530

    fix: update image paths for Vercel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -36,8 +36,8 @@ const Projects = () => {
         "Developed a dual-axis solar tracker using the 8051 microcontroller to optimize solar panel positioning for maximum sunlight exposure",
       tags: ["8051 microcontroller", "IoT", "Solidworks", "3d printing"],
       images: [
-        "portfolio-ssn/lovable-uploads/WhatsApp Image 2025-01-31 at 22.23.09_8dd2afbd.jpg",
-        "portfolio-ssn/lovable-uploads/WhatsApp Video 2025-01-31 at 22.28.41_b981b899.mp4",
+        "lovable-uploads/WhatsApp Image 2025-01-31 at 22.23.09_8dd2afbd.jpg",
+        "lovable-uploads/WhatsApp Video 2025-01-31 at 22.28.41_b981b899.mp4",
       ],
       githubLink: "https://github.com/limitleSSN/dual-axis-solar-tracker",
       liveLink: "#",
@@ -61,7 +61,7 @@ const Projects = () => {
       description:
         "Built a secure dual-password lock system requiring sequential password entry using over 30 logic gates. Utilized combinational logic and circuit design techniques to simulate reliable locking behavior",
       tags: ["Multisim"],
-      image: "portfolio-ssn/lovable-uploads/blackbg.png",
+      image: "lovable-uploads/blackbg.png",
       liveLink: "#",
       githubLink: "https://github.com/limitleSSN/2tpassword",
     },
@@ -71,7 +71,7 @@ const Projects = () => {
       description:
         "Designed and implemented an 8-bit SPI Master Controller in Verilog, deployed on a Zynq-7000 FPGA (xc7z010clg225-1L), enabling seamless interfacing with SPI-enabled devices like the ADS1258 ADC.",
       tags: ["Verilog", "Xilinx Vivado"],
-      image: "portfolio-ssn/lovable-uploads/blackbg.png",
+      image: "lovable-uploads/blackbg.png",
       liveLink: "#",
       githubLink: "#",
     },
